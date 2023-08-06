3fa8276e99a5454851f44e8ed229b0c64e971255
Daniel Rivas <de.rivasherrera@gmail.com>
2023-08-06 13:25:29 -0600

    Se realizan cambios en actualizar la cuenta y contraseña de administradores como de clientes, se finaliza el módulo informar sobre un problema

--- modules/business/admon/account.js
+++ modules/business/admon/account.js
@@ -43,7 +43,7 @@ const setUserInfo = () => {
     document.getElementById('address').value = userInformation[0].address;
     document.getElementById('email').value = userInformation[0].email;
     document.getElementById('nit').value = userInformation[0].nit;
-    document.getElementById('genderSelect').innerHTML = `<option value="1">${userInformation[0].gender == 1 ? 'Hombre' : 'Mujer'}</option>`;
+    document.getElementById('genderSelect').selectedIndex = userInformation[0].gender;
 }
 setUserInfo();
 
@@ -59,6 +59,7 @@ const updateUserData = () => {
     let address = document.getElementById('address').value;
     let email = document.getElementById('email').value;
     let nit = document.getElementById('nit').value;
+    let gender = document.getElementById('genderSelect').value;
     let err = 'Error Interno';
 
     if (userName === '' || cellPhone === '' || address === '' || email === '') {
@@ -83,8 +84,8 @@ const updateUserData = () => {
             "email": email,
             "nit": nit,
             "idrol": userInformation[0].idrol,
-            "status": userInformation[0].status,
-            "gender": userInformation[0].gender,
+            "status": 1,
+            "gender": gender,
             "user": email
         });
 
@@ -158,105 +159,6 @@ const updateUserData = () => {
                         confirmButtonText: 'Entendido'
                     });
                 }
-            }
-        }
-    }
-}
-
-
-//UPDATE STATUS OR 'DELETE' USER
-const updateStatusUser = () => {
-
-    let userInformation = atob(sessionStorage.getItem('sessionInfo'));
-    userInformation = JSON.parse(userInformation);
-    let err = 'Error Interno';
-
-    let myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer " + sessionStorage.getItem('signInToken'));
-
-    var raw = JSON.stringify({
-        "id": userInformation[0].id,
-        "fullname": userInformation[0].fullname,
-        "address": userInformation[0].address,
-        "phonenumber": userInformation[0].phonenumber,
-        "email": userInformation[0].email,
-        "nit": userInformation[0].nit,
-        "idrol": userInformation[0].idrol,
-        "status": 0,
-        "gender": userInformation[0].gender,
-        "user": userInformation[0].email
-    });
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    fetch(globalApiUrl, requestOptions)
-        .then(response => response.json())
-        .then(dataObtained => showData(dataObtained))
-        .catch(error => err = error);
-
-    const showData = (dataObtained) => {
-        if (dataObtained.body === 'Error de Servidor') {
-            Swal.fire({
-                icon: 'error',
-                title: '¡Lo Sentimos!',
-                text: 'No se pudo concretar la operación, intenta de nuevo',
-                footer: 'Si el problema persiste, por favor comunicarse con el administrador o enviar un mensaje usando la opción de soporte indicando el error.',
-                confirmButtonText: 'Entendido'
-            });
-        }
-        else {
-            if (dataObtained.status === 200 || dataObtained.status === 201 || dataObtained.status === 304) {
-                try {
-                    Swal.fire({
-                        icon: 'success',
-                        title: '¡Correcto!',
-                        text: 'La operación de completó con éxito',
-                        footer: '',
-                        showDenyButton: false,
-                        showCancelButton: false,
-                        confirmButtonText: 'Entendido',
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            sessionStorage.removeItem("signInToken");
-                            sessionStorage.removeItem("sessionInfo");
-                            window.location.href = '../../../views/g/login/component';
-                        } else if (result.isDenied) {
-                            sessionStorage.removeItem("signInToken");
-                            sessionStorage.removeItem("sessionInfo");
-                            window.location.href = '../../../views/g/login/component';
-                        }
-                        else {
-                            sessionStorage.removeItem("signInToken");
-                            sessionStorage.removeItem("sessionInfo");
-                            window.location.href = '../../../views/g/login/component';
-                        }
-                    })
-                }
-                catch (err) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: '¡Lo Sentimos!',
-                        text: 'Sa ha generado un error interno - ' + err.toUpperCase(),
-                        footer: 'Si el problema persiste, por favor comunicarse con el administrador o enviar un mensaje usando la opción de soporte indicando el error.',
-                        confirmButtonText: 'Entendido'
-                    });
-                    console.log(err);
-                }
-            }
-            else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '¡Lo Sentimos!',
-                    text: 'Sa ha generado un error interno',
-                    footer: 'Si el problema persiste, por favor comunicarse con el administrador o enviar un mensaje usando la opción de soporte indicando el error.',
-                    confirmButtonText: 'Entendido'
-                });
             }
         }
     }
